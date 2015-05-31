d3cde9f8fd17c97cfb763bcbb208b90fe8059872
Nicholas Warner <nwarner@ucsd.edu>
2015-05-31 10:10:46 -0700

    More documentation added.

--- src/Password.java
+++ src/Password.java
@@ -1,20 +1,12 @@
+import java.util.Serializable;
+
 /**
+ *<pre>
  * Purpose
  *		
  *		The Password class is meant to hold the password of a player and their profile separate
  *		from the Player itself. This helps ensure the password is not "floating around" and is
  *		very private.
- *		
- * Algorithm
- *		
- *		1. Declare the Password class
- *		2. Import the Serializable interface
- *		3. Declare the String password
- *		4. Declare a default constructor
- *		5. Declare a paramterized constructor
- *		6. Declare a method to set the password
- *		7. Declare a method to compare a given password with the object's password
- *		8. Declare the toString method and equals method
  *		
  * Structure / Process
  *		
@@ -22,40 +14,51 @@
  *		login to compare against a password input by a user, then destroyed shortly thereafter. The
  *		Password itself is stored in the filesystem as a serialized object and is therefore not
  *		in plain text.
- *		
- *	Author			- Nicholas Warner
- *	Created			- 4/24/2015
- *	Last Updated	- 5/1/2015
+ *</pre>	
+ * @author Nicholas Warner
+ * @version 5.1, May 2015
+ * @see Player
+ * @see ManageSocketConnections
  */
-
-// Import Serializable
-import java.io.Serializable;
-
-// Implement Serializable to store data as an object
 public class Password implements Serializable {
 
-	// The password itself
+	/** The password String to be saved / held. */
 	String password;
 
-	// Default Constructor
+	/** Default constructor sets the Password field to an empty String. */
     public Password() {
 
 		password = "";
     }
     
-    // Paramterized Constructor
+    /** 
+	 * Parameterized constructor takes a String argument and sets the password
+	 * field to this argument.
+	 *
+	 * @param password The new password String being set.
+	 */
     public Password(String password) {
     	
     	this.password = password;
     }
     
-    // Mutator Method
+    /**
+	 * A method to set or change a password.
+	 *
+	 * @param password The String to be set as the new Password.
+	 */
     public void setPassword(String password) {
     	
     	this.password = password;
     }
     
-    // Method to compare the given password with the object's password
+    /** 
+	 * A method to compare the given password with this Object's password.
+	 *
+	 * @param givenPassword A String to be compared against.
+	 * @return Returns true if the passwords are equal and false if they
+	 *			are not.
+	 */
     public boolean comparePassword(String givenPassword) {
     	
     	if (password.equals(givenPassword)) {
@@ -66,13 +69,23 @@ public class Password implements Serializable {
     	return false;
     }
     
-    // toString Method
+    /**
+	 * A method to show what this Object is.
+	 *
+	 * @return Returns a String indicating the Object's type.
+	 */
     public String toString() {
     	
     	return "Class: Password";
     }
     
-    // equals Method
+	/** 
+	 * A method to compare the given password with this Object's password.
+	 *
+	 * @param givenPassword A String to be compared against.
+	 * @return Returns true if the passwords are equal and false if they
+	 *          are not.
+	 */
     public boolean equals(Password onePassword) {
     	
     	if (toString().equals(onePassword.toString())) {
